45d6c28fca84c3b951ea3e3fc0c55d4e950b473a
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-04-03 18:47:26 +0530

    main.jsx File Done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,13 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store/store";
 import {Provider} from 'react-redux'
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
   <Provider store={store}>
     <App />
+    <Toaster richColors position="bottom-center" />
   </Provider>
   </BrowserRouter>
 );
